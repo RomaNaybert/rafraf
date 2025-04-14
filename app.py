48c48cf273b9adc3810fdf661b79a7c87bd52e91
Roman <sbzlatvm@mail.ru>
2025-04-14 21:55:23 +0500

    Add working app.py and requirements.txt for Timeweb

--- app.py
+++ app.py
@@ -1005,5 +1005,5 @@ def search_site():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=True)
